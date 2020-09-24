fa9d34245fe6bf54d02ca83ce8fc3fe0839d7bda
Tatiana Pashkova <tapashkova@gmail.com>
2020-09-24 15:46:48 +0300

    lesson2point4_step6 auto test answer

---
+++ lesson24_step6.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+
+try: 
+    link = "http://suninjuly.github.io/cats.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element_by_id("button")
+
+              
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
